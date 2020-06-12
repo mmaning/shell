f94c48e4b8f1cf89d7edbc82a6ab8f83eb12d281
Ma,Ning(nima) <mmaning@gmail.com>
2020-06-11 18:50:37 -0700

    rename file extension

--- reformat.sh
+++ reformat.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # for each sub directory, remove empty lines of all .txt files, then rename the .txt file to .sh
+if [[ -z $1 ]] ; then 
+echo "Pls provide folder patch to reformat"
+echo "Example: $0 /path/"
+exit 1
+fi
 
-ls | while read dir ;
+ls $1 | while read dir ;
 do 
     if [[ -d $dir ]] ; then
         for file in $dir/*.txt
         do
-        #echo $file
+        echo $file
         sed -i "" '/^$/d' $file  # remove empty lines
         b=`basename $file .txt`  
-        mv $file $dir/$b.sh           # rename file from .txt to .sh
+        mv $file $dir/$b.sh      # rename file from .txt to .sh
         done
     fi
 done
